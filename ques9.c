78e897368d1c2dccc9770d22c5ec15142ee64b88
ashwabh <49871944+ashwabh@users.noreply.github.com>
2019-04-25 00:20:12 +0530

    Update ques9.c
    fixed bugs

--- ques9.c
+++ ques9.c
@@ -89,7 +89,7 @@ int main()
 
             if(temp[i] == 0 && counter == 1) 
             { 
-                  x--; 
+                  x--;
                   printf("\nProcess[%d]\t\t%d\t\t %d\t\t %d\t\t%d", p[i], b_time[i], total - a_time[i], total - a_time[i] - b_time[i],prio[i]);
                   wait_time = wait_time + total - a_time[i] - b_time[i]; 
                   turnaround_time = turnaround_time + total - a_time[i]; 
